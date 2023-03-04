7ed511473fde0946b8eb70645201a25f8677bb7b
baiyusoup <1575727840@qq.com>
2023-03-04 20:48:39 +0800

    test: version

--- lib/puppeteer.ts
+++ lib/puppeteer.ts
@@ -1,6 +1,5 @@
 import chrome from 'chrome-aws-lambda';
 import puppeteer from 'puppeteer-core';
-let browser: puppeteer.Browser|null = null;
 
 const disabledResourceType: [string, boolean][] = [
   ["stylesheet", true],
@@ -17,6 +16,10 @@ const LOCAL_CHROME_EXECUTABLE = process.platform === "win32"
 const USER_DATA_DIR = "./node_modules/.remCache";
 const isDev = process.env.NODE_ENV === "development";
 
+declare global {
+  var _browser: puppeteer.Browser|null;
+}
+
 async function setup() {
   const options = {
     args: chrome.args,
@@ -24,16 +27,16 @@ async function setup() {
     headless: isDev ? false : chrome.headless,
     userDataDir: USER_DATA_DIR
   }
+  let browser = global._browser;
   if (!browser) {
     browser = await puppeteer.launch(options);
     browser.on("disconnected", () => {
-      browser = null;
+      global._browser = null;
     });
+    global._browser = browser;
   }
   return browser;
 }
-
-setup();
 
 export async function fetchHTML(url: string, wait?: string) {
   const browser = await setup();
@@ -48,7 +51,6 @@ export async function fetchHTML(url: string, wait?: string) {
       request.continue();
     }
   })
-  console.error(url);
   await page.goto(url);
   if (wait) {
     await page.waitForSelector(wait, { visible: true });
